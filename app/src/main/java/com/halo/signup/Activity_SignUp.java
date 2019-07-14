22f5ec6364e75ef437ec3cc9e9618eacaa939f2e
shaunhossain <shaunhossain95@gmail.com>
2019-07-14 18:55:51 +0600

    first commit

---
+++ app/src/main/java/com/halo/signup/Activity_SignUp.java
@@ -0,0 +1,13 @@
+package com.halo.signup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
